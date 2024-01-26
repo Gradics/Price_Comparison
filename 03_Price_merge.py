931ed94228bf19f9b8095d1e0a8c6c8f1291125e
Gradics <gradics@gmail.com>
2024-01-26 09:39:10 +0100

    fixes 240126

--- 03_Price_merge.py
+++ 03_Price_merge.py
@@ -3,4 +3,8 @@ dfAuchan = pd.read_pickle("dfAuchan.pkl")
 dfkifli = pd.read_pickle("dfkifli.pkl")
 PriceMerge = pd.concat([dfAuchan, dfkifli], axis=1)
 PriceMerge.to_pickle("PriceMerge.pkl")
-# print(PriceMerge)
+
+
+print(PriceMerge)
+
+
